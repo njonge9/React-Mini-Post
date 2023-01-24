cbf093f6a99699c920d532e005dfcf3269d0968e
Njonge9 <frednjonge9@gmail.com>
2023-01-24 22:06:46 +0300

    outputs list

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,34 @@
 import { useState } from "react";
 
 const Home = () => {
-  //   let name = "Njonge";
-  const [name, setName] = useState("Fred");
-  const [age, setAge] = useState(21);
-  //   A hook start with use
-
-  const handleClick = () => {
-    setName("Njonge");
-    setAge(32);
-  };
-
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new Website",
+      body: "TechCrunch is a blog that provides technology and startup news, from the latest developments in Silicon Valley to venture capital funding.",
+      author: "Njonge",
+      id: 1,
+    },
+    {
+      title: "Latest blog News",
+      body: "This blog provides news and opinion pieces on the latest technological developments in art, culture, and science for the mainstream audience.",
+      author: "Miriam",
+      id: 2,
+    },
+    {
+      title: "Top Vergeblog tips ",
+      body: "The Verge’s website homepage is vibrant – a black and white theme with bright accents of orange and magenta.",
+      author: "Fred",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>{name}</p>
-      <p>{age}</p>
-      <button onClick={handleClick}>Click me!</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
