0214e9157b7e9e79d163711213b70f5261272e36
Njonge9 <frednjonge9@gmail.com>
2023-01-24 21:11:15 +0300

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello, Njonge", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log(`hello ${name}`, e.target);
+  };
+
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={handleClick}>Click me!</button>
+      <button onClick={(e) => handleClickAgain("Njonge Fred", e)}>
+        Click me Again
+      </button>
     </div>
   );
 };
